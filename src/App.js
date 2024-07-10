17312db8f20300da23d8d9101ad1a5d35d1300a4
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-10 11:52:53 +0530

    header design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import HeaderComp from './components/HeaderComp';
 
 function App() {
   return (
     <div>
-
+      <HeaderComp />
     </div>
   );
 }
