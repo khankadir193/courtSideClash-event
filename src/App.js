9b69a58f33f44fa5210c67ee7c4265abfaa4ade9
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-10 21:11:56 +0530

    removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import HeaderComp from './components/HeaderComp';
 
 function App() {
   return (
-    <div>
+    <div className='AppContainer'>
       <HeaderComp />
     </div>
   );
 }
 
 export default App;
+
