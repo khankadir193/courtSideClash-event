b8f82f94c7c26fc9658fb30cd0f3920babe1459e
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-10 15:21:11 +0530

    infoQuestionMark is added

--- src/components/HeaderComp.js
+++ src/components/HeaderComp.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.css';
 import LangDropDownComp from './LangDropDownComp';
+import infoLangMark from '../assests/infoQuestionMark.gif';
 
 const HeaderComp = () => {
   return (
     <div className='Header'>
-        <LangDropDownComp /> 
+        <LangDropDownComp />
+        <img src={infoLangMark} alt='infoLangMark'  />
     </div>
   )
 }
