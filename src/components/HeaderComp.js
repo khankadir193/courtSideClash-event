1cb5d05ad08981e6edb99ef3597af21c93b82bb5
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-10 20:10:01 +0530

    guide component

--- src/components/HeaderComp.js
+++ src/components/HeaderComp.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import LangDropDownComp from './LangDropDownComp';
 import infoLangMark from '../assests/infoQuestionMark.gif';
+import Guide from '../popup/Guide';  // Import the Guide component
 
 const HeaderComp = () => {
+  const [showGuide, setShowGuide] = useState(false);
+
+  const toggleGuide = () => {
+    setShowGuide(!showGuide);
+  };
+
   return (
     <div className='Header'>
-        <LangDropDownComp />
-        <img src={infoLangMark} alt='infoLangMark'  />
+      <LangDropDownComp />
+      <button onClick={toggleGuide}>
+        <img src={infoLangMark} alt='infoLangMark' />
+      </button>
+      {showGuide && <Guide />} {/* Conditionally render the Guide component */}
     </div>
-  )
-}
+  );
+};
 
 export default HeaderComp;
