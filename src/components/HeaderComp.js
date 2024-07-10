77cb0720a24d46e896f1994fa3c5a6b5e789dd60
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-10 12:39:59 +0530

    langDropDownComp

--- src/components/HeaderComp.js
+++ src/components/HeaderComp.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css';
+import LangDropDownComp from './LangDropDownComp';
 
 const HeaderComp = () => {
   return (
     <div className='Header'>
-
+        <LangDropDownComp /> 
     </div>
   )
 }
---
+++ src/components/LangDropDownComp.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+// import "../styles/languageDropDown.scss";
+
+const LangDropDownComp = (props) => {
+  const { selectedLanguage, changeLanguage } = props;
+  const [isOpen, setIsOpen] = useState(false);
+  const languageOptions = ["English", "Hinglish"];
+
+  const handleLanguageChange = (index) => {
+    changeLanguage(index);
+    setIsOpen(false);
+  };
+
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div onClick={toggleDropdown} className="dropDownWrap">
+      <div className={`dropdown-box ${isOpen ? "open" : ""}`}>
+        <div className="selected-language">
+          {languageOptions[selectedLanguage]}
+        </div>
+        <svg className="dropdown-icon" viewBox="0 0 24 24">
+          <path d="M7 10l5 5 5-5z" />
+        </svg>
+      </div>
+
+      {isOpen && (
+        <div className="dropdown-menu">
+          {languageOptions.map((language, index) => (
+            <div
+              key={language}
+              className="dropdown-item"
+              onClick={() => handleLanguageChange(index)}
+            >
+              {language}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default LangDropDownComp;
