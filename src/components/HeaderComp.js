a8f1453558e9a9cd5462094f9bb735e8b473c5c8
Abhishek agarwal <abhishekagarwal151@gmail.com>
2024-07-11 09:34:58 +0530

    index.css changes

--- src/components/HeaderComp.js
+++ src/components/HeaderComp.js
@@ -4,10 +4,19 @@ import LangDropDownComp from './LangDropDownComp';
 import infoLangMark from '../assests/infoQuestionMark.gif';
 
 const HeaderComp = () => {
+
   return (
     <div className='Header'>
         <LangDropDownComp />
         <img src={infoLangMark} alt='infoLangMark'  />
+        <div className='chance-container'>
+          <div className='throws-left'>
+            Throws Left:{}
+          </div>
+          <div className='throws-sent'>
+
+          </div>
+        </div>
     </div>
   )
 }
