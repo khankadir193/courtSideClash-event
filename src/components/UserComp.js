bb7d2253d0d150b289e73be39f5498635efe473d
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-12 15:10:33 +0530

    tab-switching for daily-weekly-overall

--- src/components/UserComp.js
+++ src/components/UserComp.js
@@ -1,32 +1,120 @@
-import React from 'react';
-import dailyWeeklyBtn from '../assests/Daily-weekly-btn.png';
-import './User.css';
-import dailyBtn from '../assests/Daily-btn.png';
-import weeklyBtn from '../assests/Weekly-btn.png';
-import overAll from '../assests/Overall-btn.png';
+import React, { useState } from "react";
+// import RewardsSlider from "../../components/RewardsSlider";
+// import LeaderboardComponent from "../../components/LeaderboardComponent";
+// import { userOverall, userWeekly } from "../../api";
+import Daily from "./Daily";
+import './User.css'
 
+const UserSection = () => {
+  const [rewTabs, setRewTabs] = useState({
+    daily: true,
+    weekly: false,
+    overall: false,
+  });
 
+  const [lbTabs, setLbTabs] = useState({
+    daily: true,
+    weekly: false,
+    overall: false,
+  });
 
-const UserComp = () => {
+  const toggleRewTabs = (name) => {
+    if (name === "daily") {
+      setRewTabs({
+        daily: true,
+        weekly: false,
+        overall: false,
+      });
+    } else if (name === "weekly") {
+      setRewTabs({
+        daily: false,
+        weekly: true,
+        overall: false,
+      });
+    } else if (name === "overall") {
+      setRewTabs({
+        daily: false,
+        weekly: false,
+        overall: true,
+      });
+    }
+  };
+
+  const toggleLbTabs = (name) => {
+    if (name === "daily") {
+      setLbTabs({
+        daily: true,
+        weekly: false,
+        overall: false,
+      });
+    } else if (name === "weekly") {
+      setLbTabs({
+        daily: false,
+        weekly: true,
+        overall: false,
+      });
+    } else if (name === "overall") {
+      setLbTabs({
+        daily: false,
+        weekly: false,
+        overall: true,
+      });
+    }
+  };
   return (
-    <div className='user-container'>
-        <button className='btn-img'>
-          <img src={dailyWeeklyBtn} alt='dailyWeeklyBtn' />
-        </button>  
-      <div className='user-sub-container'>
-        <button>
-          <img src={dailyBtn} alt='dailyWeeklyBtn' />
-        </button>
-        <button>
-          <img src={weeklyBtn} alt='dailyWeeklyBtn' />
-        </button>
-        <button>
-          <img src={overAll} alt='dailyWeeklyBtn' />
-        </button>
+    <div className="user-sec">
+      <div className="rew-tabs">
+        <button
+          className={`daily ${!rewTabs.daily && "hide"}`}
+          onClick={() => toggleRewTabs("daily")}
+        />
+        <button
+          className={`weekly ${!rewTabs.weekly && "hide"}`}
+          onClick={() => toggleRewTabs("weekly")}
+        />
+
+        <button
+          className={`overall ${!rewTabs.overall && "hide"}`}
+          onClick={() => toggleRewTabs("overall")}
+        />
+      </div>
+      <div className="rewards">
+        {rewTabs.daily ? (
+          <p className="static-rew">
+            The rewards will be distributed to TOP 7 users in the ratio of the
+            Baskets scored by the users during the day.
+          </p>
+        ) : rewTabs.weekly ? (
+          <>
+            {/* <RewardsSlider rewards={userWeekly} showRanks={true}/> */}
+            <Daily />
+          </>
+        ) : (
+          <>
+            {/* <RewardsSlider rewards={userOverall} showRanks={true} /> */}
+            <Daily />
+          </>
+        )}
       </div>
 
+      <div className="rew-tabs">
+        <button
+          className={`daily ${!lbTabs.daily && "hide"}`}
+          onClick={() => toggleLbTabs("daily")}
+        />
+        <button
+          className={`weekly ${!lbTabs.weekly && "hide"}`}
+          onClick={() => toggleLbTabs("weekly")}
+        />
+
+        <button
+          className={`overall ${!lbTabs.overall && "hide"}`}
+          onClick={() => toggleLbTabs("overall")}
+        />
+      </div>
+      {/* <LeaderboardComponent isTalent={false} selTabs={lbTabs} /> */}
     </div>
   );
-}
+};
 
-export default UserComp;
+export default UserSection;
