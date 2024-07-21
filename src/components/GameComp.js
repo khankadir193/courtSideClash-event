e06c0fa685603202cdcc63dab3e6d0da945d6f95
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-21 15:40:13 +0530

    refactor and implemented GameComp

--- src/components/GameComp.js
+++ src/components/GameComp.js
@@ -3,22 +3,27 @@ import '../Style/GamePlay.css';
 import { useApi } from '../ApiCall/AppContext';
 
 const GameComp = () => {
+    // Destructure values from the useApi hook
     const { userInfo, inputValue, onChangeHandle, onUpCheck, isInputZero, thorwBtnOn, isPlaying, playGame } = useApi();
+
     return (
         <>
+            {/* Game buttons container */}
             <div className="gameBtns">
+                {/* Button to show the number of throws left */}
                 <button className="throws-left">
-                    Throws Left:{` ${Math.floor(userInfo.throwsLeft / 25000)}`}
+                    Throws Left: {` ${Math.floor(userInfo.throwsLeft / 25000)}`}
                 </button>
+
+                {/* Chances input container */}
                 <div style={{ position: "relative" }}>
                     <div className="chances">
                         <span>Chances:</span>
                         <div>
+                            {/* Input field for chances */}
                             <input
                                 id="chancesInput"
                                 className="inputField"
-                                // name="NumInput"
-                                oninput={(e) => e.target.value = (parseInt(this.value) || 0)}
                                 type="number"
                                 value={inputValue}
                                 min={1}
@@ -27,26 +32,32 @@ const GameComp = () => {
                                 onKeyUp={onUpCheck}
                                 pattern="[0-9]"
                                 style={{ border: isInputZero ? "1px solid red" : "" }}
+                                // Ensure only numbers are inputted
+                                onInput={(e) => e.target.value = (parseInt(e.target.value) || 0)}
                             />
                         </div>
                     </div>
-                    {isInputZero === true ? (
+                    {/* Display message if input value is zero */}
+                    {isInputZero && (
                         <span className="inputZero">Enter some Value</span>
-                    ) : (
-                        ""
                     )}
                 </div>
             </div>
+
+            {/* Throw button */}
             <button
-                className={thorwBtnOn === false ? "thrown" : "throw-btn"}
-                disabled={isPlaying ? true : false}
-                // disabled={true}
+                className={thorwBtnOn ? "throw-btn" : "thrown"}
+                disabled={isPlaying}
                 onClick={playGame}
             ></button>
+
+            {/* Decorative hand button */}
             <button className="hand" style={{ pointerEvents: "none" }}></button>
+
+            {/* Decorative throw button */}
             <button className="throw"></button>
         </>
-    )
+    );
 };
 
 export default GameComp;
