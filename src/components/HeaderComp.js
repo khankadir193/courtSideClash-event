61f4f0c5a56833bc1acdd3679be34f4cf8a793ff
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-07-25 16:58:00 +0530

    error has been resolved

--- src/components/HeaderComp.js
+++ src/components/HeaderComp.js
@@ -5,9 +5,14 @@ import infoLangMark from '../assests/infoQuestionMark.gif';
 import Guide from '../popup/Guide';  // Import the Guide component
 import TickerTape from './TickerTape';
 import GameComp from './GameComp';
+import { RewardHistory } from '../popup/RewardHistory';
+
+
 
 const HeaderComp = () => {
   const [showGuide, setShowGuide] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [showRewardHistory, setShowRewardHistory] = useState(0);
 
   const toggleGuide = () => {
     setShowGuide(!showGuide);
@@ -17,9 +22,13 @@ const HeaderComp = () => {
     setShowGuide(false);
   };
 
+  const toggleRewardsHistory = () => {
+    setShowRewardHistory(0);
+  };
+
   return (
     <>
-    {/* <TickerTape /> */}
+      {/* <TickerTape /> */}
       <div className='Header'>
         <LangDropDownComp />
         <TickerTape />
@@ -42,7 +51,13 @@ const HeaderComp = () => {
         <button className='guideBtn' onClick={toggleGuide}>
           <img src={infoLangMark} alt='infoLangMark' />
         </button>
+        <button
+          className="rewardBtn"
+          onClick={() => setShowRewardHistory(1)}
+          disabled={isDisabled}
+        ></button>
         {showGuide && <Guide onClose={closeGuide} />} {/* Conditionally render the Guide component */}
+        {showRewardHistory ? <RewardHistory /> : ""}
       </div>
     </>
 
---
+++ src/popup/RewardHistory.js
@@ -0,0 +1,38 @@
+import React, { useContext } from "react";
+import PopUp from "../popup/Popup.js";
+import titleBanner from "../assests/reward-records.png";
+import bg from "../assests/task-game-bg2.png";
+import "../Style/popup.css";
+import { RewardHistoryItem } from "../popup/RewardHistoryItem.js";
+import { useApi } from "../ApiCall/AppContext.js";
+
+
+export const RewardHistory = () => {
+    const {toggleRewardsHistory, rewardHistory} = useApi();
+    
+
+    return (
+        <PopUp
+            title={titleBanner}
+            bg={bg}
+            popUpHandler={toggleRewardsHistory}
+            isRewards={true}
+        >
+            <div className="rewardHistoryPopUp">
+                <div className="rewardsTitle">
+                    <div className="">Time</div>
+                    <div className="">Rewards</div>
+                </div>
+                {rewardHistory?.length ? (
+                    <div className="rewardWrapper">
+                        {rewardHistory?.map((rewardItem, index) => (
+                            <RewardHistoryItem key={index} rewardItem={rewardItem} />
+                        ))}
+                    </div>
+                ) : (
+                    <div className="noData">No Data Found!</div>
+                )}
+            </div>
+        </PopUp>
+    );
+};
