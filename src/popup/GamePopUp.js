981a333e8014ead2bcca50b324a86cae496d37bd
khankadir193 <abdul.kadir2019@vitbhopal.ac.in>
2024-08-06 10:25:59 +0530

    game-pop compoenent

--- src/popup/GamePopUp.js
+++ src/popup/GamePopUp.js
@@ -24,60 +24,153 @@ function GamePopUp(props) {
     inputValue,
     rewardWon
   } = props;
+
   const getTitle = () => {
-    if (isSuccess && !isCombo && gameErroCode === 0) {
-      return "HURRAH!!";
-    } else if (isSuccess && isCombo && gameErroCode === 0) {
-      return "GODLIKE!!";
-    } else if (!isSuccess && !isCombo && gameErroCode === 0) {
-      return "Oops!!";
-    } else if (notPoints) {
-      return "Try Again!!";
-    } else {
-      return "Oops!!";
-    }
+    const titles = {
+      0: "Oops!!",
+      1: isSuccess && !isCombo ? "HURRAH!!" : "GODLIKE!!",
+      2: "Try Again!!"
+    };
+    return titles[gameErroCode] || titles[2];
   };
+
+  const getContent = () => {
+    if (gameErroCode === 0) {
+      if (isSuccess) {
+        return (
+          <div className="game-content-success">
+            <div className="mt">
+              {isCombo ? (
+                <span>
+                  Showcasing your incredible skills, you've scored & won{" "}
+                  {rewardWon > 1 ? `${rewardWon} baskets ` : `${rewardWon} basket `}
+                  <img className="w-10 v-align-middle" src={basketIcon} alt="" /> +{" "}
+                  {rewardsContent} <img className="w-10 v-align-middle" src={bean} alt="" />. Well
+                  done!
+                </span>
+              ) : (
+                <span>
+                  That was a perfect throw and you've won {rewardsContent} <img className="w-10 v-align-middle" src={bean} alt="" />
+                </span>
+              )}
+            </div>
+          </div>
+        );
+      } else {
+        return (
+          <div className="game-content">
+            Uh-Oh! The throw was unsuccessful. Please try again. <br />
+            <span className="bottom-text">
+              <span style={{ color: "gold" }}>DO YOU KNOW?</span> Successful throws win you beans.
+            </span>
+          </div>
+        );
+      }
+    }
+
+    return (
+      <div className="game-content">
+        <div>{errMsg}</div>
+      </div>
+    );
+  };
+
+  const contentClass = isSuccess && !isCombo && gameErroCode === 0 ? "game-content-beans" : "";
 
   return (
     <PopUp
       bg={bg}
       popUpHandler={toggleGamePopUp}
-      // isRewards={true}
       textTitle={getTitle()}
       isGame={true}
     >
       <div className="game">
-        <p className={`textTitle ${getTitle() == 'GODLIKE!!' || getTitle() == 'HURRAH!!'  ? 'textTitle-congo' : '' }`} >{getTitle()}</p>
-
-        {isSuccess && !isCombo && gameErroCode === 0 ? (
-          <div className="game-content-beans">
-             <div>That was a perfect throw and you've won  {rewardsContent} <img className="w-10 v-align-middle" src={bean} alt="" /></div> 
-          </div>
-        ) : isSuccess && isCombo && gameErroCode === 0 ? (
-          <div className="game-content-success">
-           <div className="mt"> Showcasing your incredible skills, you've scored & won <br/>{rewardWon > 1 ? rewardWon+' baskets ':rewardWon+' basket '} <img className="w-10 v-align-middle" src={basketIcon} alt="" /> + { rewardsContent} <img className="w-10 v-align-middle" src={bean} alt="" />. Well done!</div>
-          </div>
-        ) : notPoints ? (
+        <p className={`textTitle ${getTitle() == "GODLIKE!!" || getTitle() == "HURRAH!!"  ? "textTitle-congo" : "" }`}>{getTitle()}</p>
+        {notPoints ? (
           <div className="game-content">
             To earn a throwing chance, spend 25k beans worth of event gifts and
             start playing. We're waiting to see you play. Come soon! DO YOU
             KNOW?
             <p className="bottom-text">Successful throws win you beans.</p>
           </div>
-        ) : !isSuccess && gameErroCode === 0 ? (
-          <div className="game-content">
-            Uh-Oh! The throw was unsuccessful. Please try again.
-            <p className="bottom-text">
-              {" "}
-              <span style={{color:'gold'}}>DO YOU KNOW?</span> Successful throws win you beans.
-            </p>
-          </div>
         ) : (
-          <div className="game-content" ><div>{errMsg}</div> </div>
+          <div className={contentClass}>{getContent()}</div>
         )}
       </div>
     </PopUp>
   );
 }
+// function GamePopUp(props) {
+//   const { toggleGamePopUp } = useApi();
+//   const {
+//     textTitle,
+//     content,
+//     beans,
+//     throwsLeft,
+//     isSuccess,
+//     isCombo,
+//     notPoints,
+//     gameErroCode,
+//     errMsg,
+//     rewardsList,
+//     rewardsContent,
+//     inputValue,
+//     rewardWon
+//   } = props;
+//   const getTitle = () => {
+//     if (isSuccess && !isCombo && gameErroCode === 0) {
+//       return "HURRAH!!";
+//     } else if (isSuccess && isCombo && gameErroCode === 0) {
+//       return "GODLIKE!!";
+//     } else if (!isSuccess && !isCombo && gameErroCode === 0) {
+//       return "Oops!!";
+//     } else if (notPoints) {
+//       return "Try Again!!";
+//     } else {
+//       return "Oops!!";
+//     }
+//   };
+
+//   return (
+//     <PopUp
+//       bg={bg}
+//       popUpHandler={toggleGamePopUp}
+//       // isRewards={true}
+//       textTitle={getTitle()}
+//       isGame={true}
+//     >
+//       <div className="game">
+//         <p className={`textTitle ${getTitle() == 'GODLIKE!!' || getTitle() == 'HURRAH!!'  ? 'textTitle-congo' : '' }`} >{getTitle()}</p>
+
+//         {isSuccess && !isCombo && gameErroCode === 0 ? (
+//           <div className="game-content-beans">
+//              <div>That was a perfect throw and you've won  {rewardsContent} <img className="w-10 v-align-middle" src={bean} alt="" /></div> 
+//           </div>
+//         ) : isSuccess && isCombo && gameErroCode === 0 ? (
+//           <div className="game-content-success">
+//            <div className="mt"> Showcasing your incredible skills, you've scored & won <br/>{rewardWon > 1 ? rewardWon+' baskets ':rewardWon+' basket '} <img className="w-10 v-align-middle" src={basketIcon} alt="" /> + { rewardsContent} <img className="w-10 v-align-middle" src={bean} alt="" />. Well done!</div>
+//           </div>
+//         ) : notPoints ? (
+//           <div className="game-content">
+//             To earn a throwing chance, spend 25k beans worth of event gifts and
+//             start playing. We're waiting to see you play. Come soon! DO YOU
+//             KNOW?
+//             <p className="bottom-text">Successful throws win you beans.</p>
+//           </div>
+//         ) : !isSuccess && gameErroCode === 0 ? (
+//           <div className="game-content">
+//             Uh-Oh! The throw was unsuccessful. Please try again.
+//             <p className="bottom-text">
+//               {" "}
+//               <span style={{color:'gold'}}>DO YOU KNOW?</span> Successful throws win you beans.
+//             </p>
+//           </div>
+//         ) : (
+//           <div className="game-content" ><div>{errMsg}</div> </div>
+//         )}
+//       </div>
+//     </PopUp>
+//   );
+// }
 
 export default GamePopUp;
